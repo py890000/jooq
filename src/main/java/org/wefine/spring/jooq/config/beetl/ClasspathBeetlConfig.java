b8be54d320ab25f43a21726138866bcbb2c70f30
wefine <wang.xiaoren@zte.com.cn>
2016-09-28 11:11:44 +0800

    update config, etc.

--- src/main/java/org/wefine/spring/jooq/config/beetl/ClasspathBeetlConfig.java
+++ src/main/java/org/wefine/spring/jooq/config/beetl/ClasspathBeetlConfig.java
@@ -4,9 +4,11 @@ import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 import org.beetl.ext.spring.BeetlSpringViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.ViewResolver;
 
+@Profile("beetl")
 @Configuration
 public class ClasspathBeetlConfig {
 
