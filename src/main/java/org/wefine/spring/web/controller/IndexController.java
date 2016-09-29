f54694d0c3890625f03dd26bd05f7967430f1f08
wefine <wang.xiaoren@zte.com.cn>
2016-09-29 15:02:32 +0800

    add spring-data, and modify logging.

--- src/main/java/org/wefine/spring/web/controller/IndexController.java
+++ src/main/java/org/wefine/spring/web/controller/IndexController.java
@@ -1,15 +1,18 @@
 package org.wefine.spring.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class DefaultController {
+@Slf4j
+public class IndexController {
 
     @GetMapping
     public String index() {
+        log.debug("welcome to index controller!");
 
         return "index";
     }
